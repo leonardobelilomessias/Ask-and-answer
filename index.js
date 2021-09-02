83219b4cd28aebcdda4db3c01bec06de8c05f2b1
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-09-02 15:17:08 -0300

    Adição do framework bootstrap

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const { render } = require("ejs");
 const express = require("express");
 const app = express();
 
 app.set('view engine','ejs')
+app.use(express.static('public'))
+
 app.get("/",(req,res)=>{
     res.render('index')
 })
-
+app.get("/perguntar",(req,res)=>{
+    res.render("perguntar")
+})
 app.listen(8080,(erro)=>{
     if(erro){
         console.log("Houve um erro")
